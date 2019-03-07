65bd38d7d3a0ef6a53fe683ef2a3c1867f60efdf
Jina Hong <jina3453@kaist.ac.kr>
2019-03-08 00:30:17 +0900

    Add reward.cpp

---
+++ reward.cpp
@@ -0,0 +1,129 @@
+/*
+https://www.swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AWgn93MasXEDFATd&contestProbId=AV15Khn6AN0CFAYD&probBoxId=AWkz_0kaIEcDFASG&type=PROBLEM&problemBoxTitle=0311&problemBoxCnt=1
+
+## sort
+- string도 begin, end 매개변수 가짐 ㅋ
+- sort함수 리턴값은 void, parameter는 2개 혹은 3개 받음. 
+- iknowmax만들려고 sort함수도 사용
+
+## dfs
+- dfs안하려고 했다가 자꾸틀려서 dfs로 품.
+-이중 포문 안에있는 dfs라서, pruing필수.
+- 이중포문 나가려고 어쩔 수 없이 flag사용. 
+
+
+- string <-> int, to_string, stoi 사용
+- string <-> char[]
+ */
+#include <iostream>
+#include <string>
+#include <string.h>//swea에선 필요하더라....흠.
+#include <algorithm>
+using namespace std;
+
+int ans;
+int iknowmax;
+bool plzEnd;
+
+/* arr에서 index idx_a와 idx_b간에 swap한 string을 반환*/
+string swap(int idx_a, int idx_b, string arr)
+{
+    char tmp = arr[idx_a];
+    arr[idx_a] = arr[idx_b];
+    arr[idx_b] = tmp;
+    return arr;
+}
+
+//arr에서 1번만 swap한 모든 경우중 최대값 리턴
+int getOneSwapMax(string arr)
+{
+    int ans = -9999;
+    for(int i=0;i<arr.size()-1;i++)
+    {
+        int tmp = stoi(swap(i,i+1, arr));
+        if(tmp > ans)
+            ans = tmp;
+    }
+    return ans;
+}
+
+/* 
+   side effect: plzEnd, ans
+
+ */
+void solve(string arr, int chance)
+{
+    /*
+    pruning:
+        chance번 swap하지 않았지만 이미 max값에 도달
+     */
+    if(stoi(arr) == iknowmax && chance > 0)
+    {
+        if(chance % 2 == 0)
+            ans = iknowmax;
+        else if (chance % 2 == 1)
+            ans = getOneSwapMax(arr);
+        plzEnd = true;
+        return;
+    }
+
+    //chance모두 다씀
+    if(chance == 0)
+    {
+        if(ans < stoi(arr))
+        {
+            ans = stoi(arr);
+        }
+        return;
+    }
+
+    for(int i = 0 ; i < arr.size() - 1 ; i++)
+    {
+        for(int j = i + 1 ; j < arr.size() ; j++)
+        {
+            if(arr[i] < arr[j])
+            {
+                string cand = swap(i,j,arr);
+                solve(cand, chance-1);
+            }
+            //pruning: 악의근원 for문빠져나오기
+            if(plzEnd) break;
+        }
+        //pruning: 악의근원 for문빠져나오기
+        if(plzEnd) break;
+    }
+}
+//arr를 내림차순으로 정렬했을 때 int값을 반환
+//arr를 여러번 swap했을때 최대값을 반환함.
+int getMax(string arr)
+{
+    sort(arr.begin() , arr.end(), greater<char>());
+    return stoi(arr);
+}
+
+//global variable 초기화
+void init(string arr)
+{
+    iknowmax  = getMax(arr);
+    ans = stoi(arr);
+    plzEnd = false;
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    for(int i=0;i<tc;i++)
+    {
+        string arr;
+        int chance;
+        cin >> arr;
+        cin >> chance;
+
+        init(arr);
+
+        solve(arr, chance);
+
+        cout << "#" << i+1 << " " << ans << endl;
+    }
+}
